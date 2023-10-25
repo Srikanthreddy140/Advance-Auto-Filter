96edefde92e5c7b59e04043415068766cef426b6
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2023-10-25 17:31:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone SRMoviesbot/Advance-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /SRMoviesbotAdvance-Auto-Filter
 fi
 cd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
